0538155c04be09a3109cb9bf5e8f21b36fcf9a11
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-08-20 09:50:19 +0300

    Commented out anchoring code.

--- exonum-java-binding/core/rust/exonum-java/src/main.rs
+++ exonum-java-binding/core/rust/exonum-java/src/main.rs
@@ -15,7 +15,7 @@
  */
 
 extern crate env_logger;
-extern crate exonum_btc_anchoring;
+//extern crate exonum_btc_anchoring; // FIXME: uncomment when anchoring fixed ECR-3468
 extern crate exonum_configuration;
 extern crate exonum_time;
 extern crate java_bindings;
